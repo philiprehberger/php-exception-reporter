5bb60d46b3d140d57fcad52d7cc43db7c7a9a7d2
Reverend Philip <me@philiprehberger.com>
2022-01-31 18:38:57 -0600

    Add withContext, setFilter, and count methods

--- tests/ExceptionReporterTest.php
+++ tests/ExceptionReporterTest.php
@@ -184,4 +184,160 @@ final class ExceptionReporterTest extends TestCase
         $this->assertSame(\LogicException::class, $report->previousClass);
         $this->assertSame('root cause', $report->previousMessage);
     }
+
+    public function test_with_context_includes_persistent_context_in_reports(): void
+    {
+        $reports = [];
+        $channel = new CallbackChannel(function (ExceptionReport $report) use (&$reports): void {
+            $reports[] = $report;
+        });
+
+        $reporter = (new ExceptionReporter)
+            ->addChannel($channel)
+            ->withContext(['request_id' => 'abc-123', 'user_id' => 42]);
+
+        $reporter->capture(new \RuntimeException('first'));
+        $reporter->capture(new \LogicException('second'));
+
+        $this->assertCount(2, $reports);
+        $this->assertSame('abc-123', $reports[0]->context['request_id']);
+        $this->assertSame(42, $reports[0]->context['user_id']);
+        $this->assertSame('abc-123', $reports[1]->context['request_id']);
+        $this->assertSame(42, $reports[1]->context['user_id']);
+    }
+
+    public function test_with_context_merges_with_per_call_context(): void
+    {
+        $captured = null;
+        $channel = new CallbackChannel(function (ExceptionReport $report) use (&$captured): void {
+            $captured = $report;
+        });
+
+        $reporter = (new ExceptionReporter)
+            ->addChannel($channel)
+            ->withContext(['request_id' => 'abc-123']);
+
+        $reporter->capture(new \RuntimeException('test'), ['action' => 'checkout']);
+
+        $this->assertSame('abc-123', $captured->context['request_id']);
+        $this->assertSame('checkout', $captured->context['action']);
+    }
+
+    public function test_with_context_returns_new_instance(): void
+    {
+        $original = new ExceptionReporter;
+        $withCtx = $original->withContext(['key' => 'value']);
+
+        $this->assertNotSame($original, $withCtx);
+    }
+
+    public function test_with_context_does_not_mutate_original(): void
+    {
+        $originalReports = [];
+        $originalChannel = new CallbackChannel(function (ExceptionReport $report) use (&$originalReports): void {
+            $originalReports[] = $report;
+        });
+
+        $original = (new ExceptionReporter)->addChannel($originalChannel);
+        $original->withContext(['request_id' => 'abc-123']);
+
+        $original->capture(new \RuntimeException('test'));
+
+        $this->assertSame([], $originalReports[0]->context);
+    }
+
+    public function test_set_filter_skips_filtered_exceptions(): void
+    {
+        $count = 0;
+        $channel = new CallbackChannel(function () use (&$count): void {
+            $count++;
+        });
+
+        $reporter = new ExceptionReporter;
+        $reporter->addChannel($channel);
+        $reporter->setFilter(fn (\Throwable $e): bool => ! $e instanceof \RuntimeException);
+
+        $reporter->capture(new \RuntimeException('filtered'));
+        $reporter->capture(new \RuntimeException('also filtered'));
+
+        $this->assertSame(0, $count);
+    }
+
+    public function test_set_filter_passes_non_filtered_exceptions(): void
+    {
+        $count = 0;
+        $channel = new CallbackChannel(function () use (&$count): void {
+            $count++;
+        });
+
+        $reporter = new ExceptionReporter;
+        $reporter->addChannel($channel);
+        $reporter->setFilter(fn (\Throwable $e): bool => ! $e instanceof \RuntimeException);
+
+        $reporter->capture(new \LogicException('allowed'));
+        $reporter->capture(new \InvalidArgumentException('also allowed'));
+
+        $this->assertSame(2, $count);
+    }
+
+    public function test_set_filter_mixed_exceptions(): void
+    {
+        $reported = [];
+        $channel = new CallbackChannel(function (ExceptionReport $report) use (&$reported): void {
+            $reported[] = $report->class;
+        });
+
+        $reporter = new ExceptionReporter;
+        $reporter->addChannel($channel);
+        $reporter->setFilter(fn (\Throwable $e): bool => ! $e instanceof \RuntimeException);
+
+        $reporter->capture(new \RuntimeException('skip'));
+        $reporter->capture(new \LogicException('keep'));
+        $reporter->capture(new \RuntimeException('skip again'));
+        $reporter->capture(new \InvalidArgumentException('keep too'));
+
+        $this->assertSame([\LogicException::class, \InvalidArgumentException::class], $reported);
+    }
+
+    public function test_count_starts_at_zero(): void
+    {
+        $reporter = new ExceptionReporter;
+
+        $this->assertSame(0, $reporter->count());
+    }
+
+    public function test_count_increments_on_capture(): void
+    {
+        $reporter = new ExceptionReporter;
+
+        $reporter->capture(new \RuntimeException('one'));
+        $reporter->capture(new \LogicException('two'));
+        $reporter->capture(new \InvalidArgumentException('three'));
+
+        $this->assertSame(3, $reporter->count());
+    }
+
+    public function test_count_does_not_increment_for_filtered_exceptions(): void
+    {
+        $reporter = new ExceptionReporter;
+        $reporter->setFilter(fn (\Throwable $e): bool => ! $e instanceof \RuntimeException);
+
+        $reporter->capture(new \RuntimeException('filtered'));
+        $reporter->capture(new \LogicException('reported'));
+
+        $this->assertSame(1, $reporter->count());
+    }
+
+    public function test_count_does_not_increment_for_deduplicated_exceptions(): void
+    {
+        $reporter = new ExceptionReporter;
+        $reporter->enableDeduplication();
+
+        $exception = new \RuntimeException('dedup');
+        $reporter->capture($exception);
+        $reporter->capture($exception);
+        $reporter->capture($exception);
+
+        $this->assertSame(1, $reporter->count());
+    }
 }
